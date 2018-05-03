1202d7c40f3e0b8b7dd1b03faf522f710e76e8dc
maxemil <max-emil.schon@icm.uu.se>
2018-05-03 15:42:54 +0200

    Update replace script to cope with non-unique substrings

--- replace_names.py
+++ replace_names.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 """replace content in multiple files by patterns in another"""
 import argparse
-import subprocess
+import shutil
+import random
 
 parser = argparse.ArgumentParser()
 
@@ -9,24 +10,70 @@ parser.add_argument("-i", "--input", required=True,
                     help="pattern, TAB separated")
 parser.add_argument("-f", "--files", required=True, nargs='+',
                     help="files that are going to be searched and replaced")
-parser.add_argument("-b", "--backup", required=False, default=False,
+parser.add_argument("-b", action='store_true',
                     help="switch for creating backup file with extension .bak")
 
 args = parser.parse_args()
 
 
-def main(inputfile, replacementfiles, backup):
-    with open(inputfile) as f:
-        patterns = set()
-        for line in f:
-            line = line.strip().split('\t')
-            patterns.add('s/%s/%s/g' % (line[0], line[1]))
-        if backup:
-            subprocess.call(
-                ['sed', '-r', '-i.bak', ";".join(patterns)] + replacementfiles)
+def replace(string, patterns):
+    for k,v in patterns.items():
+        string = string.replace(k, v)
+    return string
+
+
+def safe_replace(string, patterns):
+    for p in list(patterns):
+        if not any([p in key for key in patterns.keys() if not p is key]):
+            string = string.replace(p, patterns[p])
+            patterns.pop(p)
+
+    while patterns:
+        p = random.choice(list(patterns.keys()))
+        if any([p in key for key in patterns.keys() if not p is key]):
+            pass
         else:
-            subprocess.call(
-                ['sed', '-r', '-i', ";".join(patterns)] + replacementfiles)
+            string = string.replace(p, patterns[p])
+            patterns.pop(p)
+    return string
+
+
+def read_patterns(inputfile):
+    patterns = {}
+    for line in open(inputfile):
+        line = line.strip().split('\t')
+        patterns[line[0]] = line[1]
+    return patterns
+
+
+def need_safe_replace(patterns):
+    for p in sorted(patterns.keys()):
+        p_old = p
+        if p_old in p:
+            print("WARNING: patterns are not unique or substrings of each other! Will attempt a safe replace.")
+            return True
+    return False
+
+
+def main(inputfile, replacementfiles, backup):
+    if backup:
+        for refile in replacementfiles:
+            shutil.copyfile(refile, "%s.bak" % refile)
+    else:
+        pass
+
+    patterns = read_patterns(inputfile)
+    safe = need_safe_replace(patterns)
+
+    for refile in replacementfiles:
+        string = open(refile).read()
+        if safe:
+            string = safe_replace(string, patterns)
+        else:
+            string = replace(string, patterns)
+        with open(refile, 'w') as out:
+            out.write(string)
+
 
 if __name__ == "__main__":
-    main(args.input, args.files, args.backup)
+    main(args.input, args.files, args.b)
