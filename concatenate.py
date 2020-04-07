9ddc1ebcc806db07d18b41f004f0a927560c0d14
maxemil <max-emil.schon@icm.uu.se>
2020-04-07 13:24:09 +0200

    Add partitions and species name separator to script

--- concatenate.py
+++ concatenate.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from Bio import AlignIO, SeqIO, Seq, SeqRecord
+from collections import OrderedDict
 import Bio
 import argparse
 import pandas as pd
@@ -8,13 +9,19 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('fasta', type=str, nargs='+',
-	help='fasta files to be concatenated')
+    help='fasta files to be concatenated')
 parser.add_argument('-out', type=str, default='concat.fasta',
         help='if no output file is given, print alignment to concat.fasta')
 parser.add_argument('-s', action='store_true',
         help='get basic stats for alignments')
 parser.add_argument('-t', '--threshold', type=int, default=1,
         help='minimum number of genes that must be present in an organism')
+parser.add_argument('-ps', '--partition_size', type=int, default=0,
+        help='size of the partitions')
+parser.add_argument('-p', '--partitions', action='store_true',
+        help='print a partitions file in RAxML style')
+parser.add_argument('-sep', '--seperator', default='_',
+        help='seperator between species and gene id')
 args = parser.parse_args()
 
 def main(args):
@@ -32,11 +39,11 @@ def main(args):
             print('alignment file %s not readable' % f)
 
         for rec in SeqIO.parse(f, 'fasta'):
-            if rec.id in df.index:
-                df[f].loc[rec.id] = str(rec.seq)
+            if rec.id.split(args.seperator)[0] in df.index:
+                df[f].loc[rec.id.split(args.seperator)[0]] = str(rec.seq)
             else:
-                df.loc[rec.id] = pd.Series()
-                df[f].loc[rec.id] = str(rec.seq)
+                df.loc[rec.id.split(args.seperator)[0]] = pd.Series(dtype=str)
+                df[f].loc[rec.id.split(args.seperator)[0]] = str(rec.seq)
     if args.s:
         for col in df:
             print(col)
@@ -56,6 +63,34 @@ def main(args):
                                       seq=Seq.Seq(''.join(row[1:])))
             SeqIO.write(rec, out, 'fasta')
 
+    if args.partitions:
+        partitions = OrderedDict()
+        start = 1
+        for i, gene in enumerate(df.iteritems()):
+            if args.partition_size > 0:
+                p_count = 1
+                gene_end = start + len(gene[1][0]) - 1
+                while start + args.partition_size < gene_end:
+                    end = start + args.partition_size
+                    partitions[gene[0] + str(p_count)] = (start, end)
+                    start = end + 1
+                    p_count += 1
+                if gene_end - start < 30:
+                    start, end = partitions[gene[0] + str(p_count - 1)]
+                    partitions[gene[0] + str(p_count - 1)] = (start, gene_end)
+                else:
+                    partitions[gene[0] + str(p_count)] = (start, gene_end)
+                start = gene_end + 1
+            else:
+                end = start + len(gene[1][0]) - 1
+                partitions[gene[0] + str(p_count)] = (start, end)
+                start = end + 1
+        with open("{}.partitions".format(args.out), 'w') as pout:
+            print("#nexus", file=pout)
+            print("begin sets;", file=pout)
+            for k, v in partitions.items():
+                print("charset {} = {}-{};".format(k, v[0], v[1]), file=pout)
+            print("end;", file=pout)
 
 if __name__ == '__main__':
     main(args)
