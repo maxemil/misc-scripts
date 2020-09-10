76381298dec14cb1501f711b2233bbb86b2cb4a1
maxemil <max-emil.schon@icm.uu.se>
2020-09-10 10:44:11 +0200

    add taxonomy option to color tree script

--- color_tree.py
+++ color_tree.py
@@ -1,5 +1,6 @@
 import argparse
 import ete3
+import re
 
 def parse_colors(tree, colors):
     tax2color = {}
@@ -18,6 +19,22 @@ def get_color_taxon(name, tax2color):
     else:
         return "#000000"
 
+def get_taxonomy(tree, tax2color):
+    ncbi = ete3.ncbi_taxonomy.NCBITaxa()
+    for l in tree.get_leaves():
+        if re.match("^[0-9]+", l.name):
+            lin = ncbi.get_lineage(int(l.name.split('.')[0]))
+            tax = []
+            for t in lin: 
+                name = ncbi.get_taxid_translator([t])[t].replace(' ', '_')
+                if not name in ['root', 'cellular_organisms']:
+                    tax.append(name)
+            tax = ";".join(tax)
+            col = get_color_taxon(l.name, tax2color)
+            l.name = tax
+            tax2color[l.name] = col
+            
+                
 def get_taxa_block_nexus(tree, tax2color):
     taxa_block = "begin taxa;\n"
     taxa_block += "\tdimensions ntax={};\n".format(len(tree.get_leaves()))
@@ -45,14 +62,16 @@ def write_nexus(outfile, tree, tax2color):
         print(get_taxa_block_nexus(tree, tax2color), file=out)
         print('begin trees;', file=out)
         print('\ttree tree_1 = [&R]', file=out)
-        print("\t{}".format(tree.write(format=2)), file=out)
+        print("\t{}".format(tree.write(format=0)), file=out)
         print('end;', file=out)
         print(get_figtree_block_nexus(), file=out)
 
-def main(treefile, outfile, colors):
-    tree = ete3.PhyloTree(treefile, format=2)
+def main(treefile, outfile, colors, taxonomy):
+    tree = ete3.PhyloTree(treefile, format=0)
     tree.ladderize(direction=1)
     tax2color = parse_colors(tree, colors)
+    if taxonomy:
+        get_taxonomy(tree, tax2color)
     write_nexus(outfile, tree, tax2color)
 
 parser = argparse.ArgumentParser()
@@ -62,6 +81,8 @@ parser.add_argument("-c", "--colors", required=True,
                     help="input file with color codes for taxa, as a tab separated list")
 parser.add_argument("-o", "--output", required=False, default="tree.nex",
                     help="output file in nexus format, prepared for FigTree")
+parser.add_argument("-t", "--taxonomy", action='store_true',
+                    help="parse EggNOG style seq names to taxonomy strings")
 args = parser.parse_args()
 
 if __name__ == '__main__':
