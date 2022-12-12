c25bd65811c143a6e96f85415ebfd2024ea18ff5
Max Emil Schoen <9975295+maxemil@users.noreply.github.com>
2022-12-12 10:22:29 +0100

    Add option to provide short names for tips

--- color_tree.py
+++ color_tree.py
@@ -22,6 +22,20 @@ def get_color_taxon(name, tax2color):
     else:
         return cs.rgb2hex('black')
 
+def parse_short_names(short_names):
+    id2name = {}
+    if short_names:
+        for line in open(short_names):
+            line = line.strip().split():
+            id2name[line[0]] = line[1]
+    return id2name
+
+def get_short_name(leaf, id2name):
+    if leaf.name in id2name:
+        return id2name[leaf.name]
+    else:
+        return leaf.name
+
 def get_taxonomy(tree, tax2color):
     ncbi = ete3.ncbi_taxonomy.NCBITaxa()
     for l in tree.get_leaves():
@@ -39,12 +53,12 @@ def get_taxonomy(tree, tax2color):
             tax2color[l.name] = col
             
                 
-def get_taxa_block_nexus(tree, tax2color):
+def get_taxa_block_nexus(tree, tax2color, id2name):
     taxa_block = "begin taxa;\n"
     taxa_block += "\tdimensions ntax={};\n".format(len(tree.get_leaves()))
     taxa_block += "\ttaxlabels\n"
     for t in tree.get_leaves():
-        taxa_block += "\t{}[&!color={}]\n".format(t.name, get_color_taxon(t.name, tax2color))
+        taxa_block += "\t{}[&!color={}]\n".format(get_short_name(t, id2name), get_color_taxon(t.name, tax2color))
     taxa_block += ";\n"
     taxa_block += "end;\n"
     return taxa_block
@@ -60,23 +74,24 @@ begin figtree;
 end;
 """
 
-def write_nexus(outfile, tree, tax2color):
+def write_nexus(outfile, tree, tax2color, id2name):
     with open(outfile, 'w') as out:
         print('#NEXUS', file=out)
-        print(get_taxa_block_nexus(tree, tax2color), file=out)
+        print(get_taxa_block_nexus(tree, tax2color, id2name), file=out)
         print('begin trees;', file=out)
         print('\ttree tree_1 = [&R]', file=out)
         print("\t{}".format(tree.write(format=0)), file=out)
         print('end;', file=out)
         print(get_figtree_block_nexus(), file=out)
 
-def main(treefile, outfile, colors, taxonomy):
+def main(treefile, outfile, colors, taxonomy, short_names):
     tree = ete3.PhyloTree(treefile, format=0)
     tree.ladderize(direction=1)
     tax2color = parse_colors(tree, colors)
     if taxonomy:
         get_taxonomy(tree, tax2color)
-    write_nexus(outfile, tree, tax2color)
+    id2name = parse_short_names(short_names)
+    write_nexus(outfile, tree, tax2color, id2name)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True,
@@ -87,7 +102,9 @@ parser.add_argument("-o", "--output", required=False, default="tree.nex",
                     help="output file in nexus format, prepared for FigTree")
 parser.add_argument("-t", "--taxonomy", action='store_true',
                     help="parse EggNOG style seq names to taxonomy strings")
+parser.add_argument("-s", "--short_names", required=False, default=None,
+                    help="table with two columns with rec ids and short names")
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    main(args.input, args.output, args.colors, args.taxonomy)
+    main(args.input, args.output, args.colors, args.taxonomy, args.short_names)
